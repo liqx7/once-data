fe1a83580530db86200315fd3ceb3e152ac1cfec
“liqinxin” <liqinxin@hyperchain.cn>
2019-12-13 14:19:19 +0800

    resource

--- src/pages/Proj/Resource/index.jsx
+++ src/pages/Proj/Resource/index.jsx
@@ -1,22 +1,114 @@
 import React from 'react';
+import {
+  // G2,
+  Chart,
+  Geom,
+  Axis,
+  Tooltip,
+  Coord,
+  Label,
+  // Legend,
+  // View,
+  // Guide,
+  // Shape,
+  // Facet,
+  // Util,
+} from 'bizcharts';
+import DataSet from '@antv/data-set';
+import { Button } from 'antd';
+
 // import { PageHeaderWrapper } from '@ant-design/pro-layout';
 // import { FormattedMessage } from 'umi-plugin-react/locale';
 // import { Card, Typography, Alert } from 'antd';
 // import styles from './Welcome.less';
 
-class Calc extends React.Component {
+class Resource extends React.Component {
   constructor() {
     super();
-    this.state = {
-      title: 'calc',
-    };
+    this.state = {};
   }
 
   render() {
-    const { title } = this.state;
+    const { DataView } = DataSet;
+    const data = [
+      {
+        item: '已用',
+        count: 40,
+      },
+      {
+        item: '未用',
+        count: 21,
+      },
+    ];
+    const dv = new DataView();
+    dv.source(data).transform({
+      type: 'percent',
+      field: 'count',
+      dimension: 'item',
+      as: 'percent',
+    });
+    const cols = {
+      percent: {
+        formatter: val => `${val * 100}%`,
+      },
+    };
 
-    return <div>{title}</div>;
+    return (
+      <div>
+        <section>
+          <h1>hdfs</h1>
+          <Button>申请</Button>
+          <div>
+            <Chart
+              height={window.innerHeight}
+              data={dv}
+              scale={cols}
+              padding={[80, 100, 80, 80]}
+              forceFit
+            >
+              <Coord type="theta" radius={0.75} />
+              <Axis name="percent" />
+              {/* <Legend
+              position="right"
+              offsetY={-window.innerHeight / 2 + 120}
+              offsetX={-100}
+            /> */}
+              <Tooltip
+                showTitle={false}
+                itemTpl='<li><span style="background-color:{color};" class="g2-tooltip-marker"></span>{name}: {value}</li>'
+              />
+              <Geom
+                type="intervalStack"
+                position="percent"
+                color="item"
+                tooltip={[
+                  'item*percent',
+                  (item, percent) => {
+                    percent = `${percent * 100}%`;
+                    return {
+                      name: item,
+                      value: percent,
+                    };
+                  },
+                ]}
+                style={{
+                  lineWidth: 1,
+                  stroke: '#fff',
+                }}
+              >
+                <Label content="percent" formatter={(val, item) => `${item.point.item}: ${val}`} />
+              </Geom>
+            </Chart>
+          </div>
+        </section>
+
+        <section>
+          <h1>yarn队列</h1>
+          <Button>申请</Button>
+        </section>
+      </div>
+    );
   }
 }
 
-export default Calc;
+export default Resource;
